75a970e999517b1b2d94509a866209ddb13bd068
alexeyDvnik <dv.nik89@gmail.com>
2023-08-01 23:12:44 +0500

    feat: complit task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+	let result = ('' + n).
+	split('').
+	reduce( (resArr, current, index, arr) => {
+		if ( (index === 0 && current === '-') || (index ===  arr.length - 1 && current === '0') ) {
+			return resArr;
+		} else {
+			resArr.unshift(current);
+			return resArr;
+		}
+	}, [] ).
+	join('');
+	return +result;
 }
